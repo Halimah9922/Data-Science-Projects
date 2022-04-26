4fc7ee0b42f5f3636b2342aef3b97f244b7e8087
Halimah Abubakar-sidiq <100470319+Halimah9922@users.noreply.github.com>
2022-04-26 20:36:20 +0100

    Add files via upload
    Time series visualizer project

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # This entrypoint file to be used in development. Start by reading README.md
-import sea_level_predictor
+import time_series_visualizer
 from unittest import main
 
 # Test your function by calling it here
-sea_level_predictor.draw_plot()
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
